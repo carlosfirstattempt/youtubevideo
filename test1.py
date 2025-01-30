6644bf2af13896f50a686012430be897e5278776
carlosfirstattempt <cjrjay03@gmail.com>
2025-01-30 11:40:39 -0500

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+total = [1, 2, 3, 4]
+
+def totaltot():
+    for num in range():
+        sum * total
+    
+    
+#if i want to multiple something
+# i can use *= but i have to have like a list then make a new
+#value in order to multiple tjem, initilzase the other random
+#value after making a function
